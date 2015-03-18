b1762e2710b82982477c5c088bd0fc96f1678a20
Mike Green <mike@fifthroomcreative.com>
2015-03-18 11:29:42 -0400

    added ssh config to configuration object, stubbed the main gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,59 @@
 'use strict';
 
 var _           = require('lodash');
+var path        = require('path');
 var gulp        = require('gulp');
 var $           = require('gulp-load-plugins');
 var runSequence = require('run-sequence');
 
 var config      = {
   dev: {
-    host: '127.0.0.1',
-    port: 8889,
-    user: 'root',
-    password: 'root'
+    mysql: {
+      host: '127.0.0.1',
+      port: 8889,
+      user: 'root',
+      password: 'root'
+    },
+    ssh: {
+      host: '127.0.0.1',
+      user: 'vagrant',
+      port: 2222,
+      privateKey: path.join(__dirname, '.vagrant', 'machines', 'default', 'virtualbox', 'private_key')
+    }
   },
   staging: {
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: 's3cur3'
+    mysql: {
+      host: '127.0.0.1',
+      port: 3306,
+      user: 'root',
+      password: 'secure123'
+    },
+    ssh: {
+      host: 'myserver.com',
+      port: 22,
+      user: 'sshuser',
+      privateKey: path.join(process.env.HOME, '.ssh', 'id_rsa'),
+      wpCLI: '/home/sshuser/bin/wp'
+    }
   }
 };
+
+gulp.task('db:up', function(callback) {
+  runSequence(
+    'db:up:dump',
+    'db:up:copy',
+    'db:up:load',
+    'db:up:cleanup',
+    callback
+  );
+});
+
+gulp.task('db:down', function(callback) {
+  runSequence(
+    'db:down:dump',
+    'db:down:copy',
+    'db:down:load',
+    'db:down:cleanup',
+    callback
+  );
+});
